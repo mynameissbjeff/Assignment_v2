c83a568f82b8611739ca355bc15c128c4cfc39b7
mynameissbjeff <33080914+mynameissbjeff@users.noreply.github.com>
2017-12-18 22:17:21 +0800

    UI

--- app/src/main/java/my/edu/taruc/lab22profile/ModeActivity.java
+++ app/src/main/java/my/edu/taruc/lab22profile/ModeActivity.java
@@ -1,16 +1,25 @@
 package my.edu.taruc.lab22profile;
 
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class ModeActivity extends AppCompatActivity {
+
+    private static final int REQUEST_PRACTICE_MODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         setContentView(R.layout.activity_mode);
+    }
+
+    public void Practicemode(View view){
+        Intent intent = new Intent(this,PracticeActivity.class);
+        startActivityForResult(intent, REQUEST_PRACTICE_MODE);
     }
 
 }
