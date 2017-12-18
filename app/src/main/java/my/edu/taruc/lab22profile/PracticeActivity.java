d25fbba21785ea2e7325a47a986ffaa90431100e
mynameissbjeff <33080914+mynameissbjeff@users.noreply.github.com>
2017-12-19 00:45:23 +0800

    UI

--- app/src/main/java/my/edu/taruc/lab22profile/PracticeActivity.java
+++ app/src/main/java/my/edu/taruc/lab22profile/PracticeActivity.java
@@ -1,5 +1,6 @@
 package my.edu.taruc.lab22profile;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,10 +11,12 @@ import java.util.Random;
 
 public class PracticeActivity extends AppCompatActivity {
 
+    private static final int REQUEST_MAIN_MENU = 1;
+
     public TextView textViewNum;
     public  TextView textViewQuestion;
     public Button buttonA;
-    public int countNum = 0;
+    public int countNum = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,10 +46,17 @@ public class PracticeActivity extends AppCompatActivity {
 
     }
 
-    public void buttonAans(){
-        //countnum++;
-        //textViewNum.setText(""+countnum);
-        //CreateQuestion();
+    public void buttonAans(View view){
+        if(countNum<12){
+            countNum++;
+            textViewNum.setText(""+countNum);
+            CreateQuestion();
+        }
+        else if(countNum >= 12){
+            Intent intent = new Intent(this,MainActivity.class);
+            startActivityForResult(intent, REQUEST_MAIN_MENU);
+        }
+
     }
 
 }
