48e051b3212ed004f5bb0e8a2667c3082b374c42
mynameissbjeff <33080914+mynameissbjeff@users.noreply.github.com>
2017-12-19 00:21:51 +0800

    UI

--- app/src/main/java/my/edu/taruc/lab22profile/PracticeActivity.java
+++ app/src/main/java/my/edu/taruc/lab22profile/PracticeActivity.java
@@ -3,6 +3,7 @@ package my.edu.taruc.lab22profile;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.Random;
@@ -11,6 +12,8 @@ public class PracticeActivity extends AppCompatActivity {
 
     public TextView textViewNum;
     public  TextView textViewQuestion;
+    public Button buttonA;
+    public int countNum = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,19 +22,31 @@ public class PracticeActivity extends AppCompatActivity {
 
         textViewNum = (TextView)findViewById(R.id.textViewNum);
         textViewQuestion = (TextView)findViewById(R.id.textViewQuestion);
+        buttonA = (Button)findViewById(R.id.buttonA);
         CreateQuestion();
-        //textViewQuestion.setText("LOL");
+        textViewNum.setText(""+countNum);
     }
 
 
     void CreateQuestion(){
         int min = 1;
         int max = 30;
+        int ans;
 
         Random r = new Random();
         int num1 = r.nextInt(max - min + 1) + min;
 
         int num2 = r.nextInt(max - min + 1) + min;
         textViewQuestion.setText(num1 + "+" + num2 + " = ?");
+        ans = num1 + num2;
+        buttonA.setText("A. "+ans);
+
     }
+
+    public void buttonAans(){
+        //countnum++;
+        //textViewNum.setText(""+countnum);
+        //CreateQuestion();
+    }
+
 }
