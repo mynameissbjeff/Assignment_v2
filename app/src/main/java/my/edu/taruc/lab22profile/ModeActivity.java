1bf44d78396c90198c6e34009f70e66593b0a13b
mynameissbjeff <33080914+mynameissbjeff@users.noreply.github.com>
2017-12-19 23:21:33 +0800

    UI

--- app/src/main/java/my/edu/taruc/lab22profile/ModeActivity.java
+++ app/src/main/java/my/edu/taruc/lab22profile/ModeActivity.java
@@ -17,9 +17,45 @@ public class ModeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_mode);
     }
 
-    public void Practicemode(View view){
+    public void Practicemode(Bundle bundle){
         Intent intent = new Intent(this,PracticeActivity.class);
+        intent.putExtras(bundle);
         startActivityForResult(intent, REQUEST_PRACTICE_MODE);
+    }
+
+    public void Additional(View view){
+        String i="ADD";
+        Bundle bundle = new Bundle();
+        bundle.putString("operation", i);
+        Practicemode(bundle);
+    }
+
+    public void Subtraction(View view){
+        String i="SUB";
+        Bundle bundle = new Bundle();
+        bundle.putString("operation", i);
+        Practicemode(bundle);
+    }
+
+    public void Multiplication(View view){
+        String i="MUL";
+        Bundle bundle = new Bundle();
+        bundle.putString("operation", i);
+        Practicemode(bundle);
+    }
+
+    public void Division(View view){
+        String i="DIV";
+        Bundle bundle = new Bundle();
+        bundle.putString("operation", i);
+        Practicemode(bundle);
+    }
+
+    public void Mix(View view){
+        String i="MIX";
+        Bundle bundle = new Bundle();
+        bundle.putString("operation", i);
+        Practicemode(bundle);
     }
 
 }
--- app/src/main/java/my/edu/taruc/lab22profile/PracticeActivity.java
+++ app/src/main/java/my/edu/taruc/lab22profile/PracticeActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.text.DecimalFormat;
 import java.util.Random;
 
 public class PracticeActivity extends AppCompatActivity {
@@ -16,6 +17,9 @@ public class PracticeActivity extends AppCompatActivity {
     public TextView textViewNum;
     public  TextView textViewQuestion;
     public Button buttonA;
+    public Button buttonB;
+    public Button buttonC;
+    public Button buttonD;
     public int countNum = 1;
 
     @Override
@@ -26,6 +30,9 @@ public class PracticeActivity extends AppCompatActivity {
         textViewNum = (TextView)findViewById(R.id.textViewNum);
         textViewQuestion = (TextView)findViewById(R.id.textViewQuestion);
         buttonA = (Button)findViewById(R.id.buttonA);
+        buttonB = (Button)findViewById(R.id.buttonB);
+        buttonC = (Button)findViewById(R.id.buttonC);
+        buttonD = (Button)findViewById(R.id.buttonD);
         CreateQuestion();
         textViewNum.setText(""+countNum);
     }
@@ -33,17 +40,74 @@ public class PracticeActivity extends AppCompatActivity {
 
     void CreateQuestion(){
         int min = 1;
-        int max = 30;
-        int ans;
+        int min2 = 0;
+        int max = 15;
+        float ans;
+
+        Bundle bundle = getIntent().getExtras();
+        String operation= bundle.getString("operation");
 
         Random r = new Random();
         int num1 = r.nextInt(max - min + 1) + min;
+        int num2 = r.nextInt(num1 - min2 + 1) + min;
+        int ansnum = r.nextInt(4-1+1)+1;
 
-        int num2 = r.nextInt(max - min + 1) + min;
-        textViewQuestion.setText(num1 + "+" + num2 + " = ?");
-        ans = num1 + num2;
-        buttonA.setText("A. "+ans);
+        ans = getAnswer(operation,num1,num2);
 
+        setAnswer(ansnum,ans);
+    }
+
+    public float getAnswer(String operation,float num1,float num2){
+        float ans = 0;
+        float mix = randomAns(1,4);
+        if(operation.equals("ADD")||(operation.equals("MIX")&&mix==1)){
+            textViewQuestion.setText(num1 + "+" + num2 + " = ?");
+            ans = num1 + num2;
+        }
+        else if(operation.equals("SUB")||(operation.equals("MIX")&&mix==2)){
+            textViewQuestion.setText(num1 + "-" + num2 + " = ?");
+            ans = num1 - num2;
+        }
+        else if(operation.equals("MUL")||(operation.equals("MIX")&&mix==3)){
+            textViewQuestion.setText(num1 + "X" + num2 + " = ?");
+            ans = num1 * num2;
+        }
+        else if(operation.equals("DIV")||(operation.equals("MIX")&&mix==4)){
+            textViewQuestion.setText(num1 + "/" + num2 + " = ?");
+            ans = num1 / num2;
+        }
+        DecimalFormat decimalFormat = new DecimalFormat("#.##");
+        ans = Float.valueOf(decimalFormat.format(ans));
+        return ans;
+    }
+
+    public void setAnswer(int ansnum,float ans){
+        //buttonA.setText("A. "+randomAns(0,30));
+        //buttonB.setText("B. "+randomAns(0,30));
+        //buttonC.setText("C. "+randomAns(0,30));
+        //buttonD.setText("D. "+randomAns(0,30));
+        buttonA.setText("A. "+randomAns((int)ans-10,(int)ans+10));
+        buttonB.setText("B. "+randomAns((int)ans-10,(int)ans+10));
+        buttonC.setText("C. "+randomAns((int)ans-10,(int)ans+10));
+        buttonD.setText("D. "+randomAns((int)ans-10,(int)ans+10));
+        if(ansnum == 1){
+            buttonA.setText("A. "+ans);
+        }
+        else if(ansnum == 2){
+            buttonB.setText("B. "+ans);
+        }
+        else if(ansnum == 3){
+            buttonC.setText("C. "+ans);
+        }
+        else if(ansnum == 4){
+            buttonD.setText("D. "+ans);
+        }
+    }
+
+    public float randomAns(int min,int max){
+        Random r = new Random();
+        float randans = r.nextInt(max - min + 1) + min;
+        return randans;
     }
 
     public void buttonAans(View view){
@@ -55,6 +119,7 @@ public class PracticeActivity extends AppCompatActivity {
         else if(countNum >= 12){
             Intent intent = new Intent(this,MainActivity.class);
             startActivityForResult(intent, REQUEST_MAIN_MENU);
+            finish();
         }
 
     }
